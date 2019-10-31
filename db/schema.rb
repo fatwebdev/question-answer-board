4e1b4b5b3653055df4250f4c62f42001fda2e8d8
Yan Shevtsov <fatwebdev@gmail.com>
2019-10-31 13:48:12 +0300

    add Question model presence constraint

--- db/schema.rb
+++ db/schema.rb
@@ -16,8 +16,8 @@ ActiveRecord::Schema.define(version: 2019_10_31_101549) do
   enable_extension "plpgsql"
 
   create_table "questions", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
+    t.string "title", null: false
+    t.text "body", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
