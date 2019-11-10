7abc3ee3a7bbdac9908ecbcdb1aa3b02e1c7ffc7
Yan Shevtsov <fatwebdev@gmail.com>
2019-11-10 10:59:34 +0300

    refactor user can sign in spec

--- spec/features/users/sign_in_spec.rb
+++ spec/features/users/sign_in_spec.rb
@@ -5,7 +5,7 @@ feature 'User can sign in', '
   As an unauthenticated user
   I\'d like to be able to sign in
 ' do
-  given!(:user) { User.create!(email: 'user@test.com', password: '12345678') }
+  given!(:user) { create(:user) }
 
   background do
     visit root_path
@@ -13,8 +13,8 @@ feature 'User can sign in', '
   end
 
   scenario 'Registered user tries to sign in' do
-    fill_in 'Email', with: 'user@test.com'
-    fill_in 'Password', with: '12345678'
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
     click_on 'Log in'
 
     expect(page).to have_content 'Signed in successfully'
