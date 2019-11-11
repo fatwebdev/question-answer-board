3e08ca186e4b61df6c253ee35e4c97c875d881b9
Yan Shevtsov <fatwebdev@gmail.com>
2019-11-11 23:15:15 +0300

    add answer controller spec for diff type users

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe AnswersController, type: :controller do
     before { login(author) }
 
     context 'with valid attributes' do
-      it 'saves a new question answer in the database' do
+      it 'saves a new question answer' do
         expect { subject }.to change(question.answers, :count).by(1)
       end
 
@@ -24,20 +24,24 @@ RSpec.describe AnswersController, type: :controller do
     context 'with invalid attributes' do
       let(:params) { { question_id: question, answer: attributes_for(:answer, :invalid) } }
 
-      it 'does not save the questions answer' do
+      it 'does not save a new question answer' do
         expect { subject }.to_not change(question.answers, :count)
       end
 
-      it 're-render show question view' do
+      it 're-renders show question view' do
         expect(subject).to render_template 'questions/show'
       end
     end
 
-    context 'unathenticate user' do
+    context 'as not authenticated user' do
       before { sign_out(user) }
 
-      it 'try saves a new answer in the database' do
+      it 'does not saves a new question answer' do
         expect { subject }.to_not change(question.answers, :count)
+      end
+
+      it 'redirects to sign in' do
+        expect(subject).to redirect_to new_user_session_path
       end
     end
   end
@@ -78,21 +82,38 @@ RSpec.describe AnswersController, type: :controller do
   describe 'DELETE #destroy' do
     let!(:answer) { create(:answer, question: question, user: author) }
     subject { delete :destroy, params: { id: answer } }
-    before { login(author) }
 
-    it 'delete the question answer' do
-      expect { subject }.to change(question.answers, :count).by(-1)
+    context 'as author' do
+      before { login(author) }
+
+      it 'deletes the question answer' do
+        expect { subject }.to change(question.answers, :count).by(-1)
+      end
+
+      it 'redirects to index' do
+        expect(subject).to redirect_to question
+      end
     end
 
-    it 'redirect to index' do
-      expect(subject).to redirect_to question
-    end
-
-    context 'user tries to remove not its answer' do
+    context 'as user' do
       before { login(user) }
 
-      it 'delete the answer' do
+      it 'does not delete the question answer' do
         expect { subject }.to_not change(Answer, :count)
+      end
+
+      it 'redirects to index' do
+        expect(subject).to redirect_to question
+      end
+    end
+
+    context 'as not authenticated user' do
+      it 'does not delete the question answer' do
+        expect { subject }.to_not change(Question, :count)
+      end
+
+      it 'redirects to sign in' do
+        expect(subject).to redirect_to new_user_session_path
       end
     end
   end
