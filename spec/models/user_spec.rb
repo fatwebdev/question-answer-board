11f8fb8061a3c3e61f44ef36d4ea3d1429c306dc
Yan Shevtsov <fatwebdev@gmail.com>
2019-11-11 12:21:05 +0300

    add spec for User#author_of?

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,13 @@ RSpec.describe User, type: :model do
 
   it { should have_many :questions }
   it { should have_many :answers }
+
+  let(:author) { create(:user) }
+  let(:user) { create(:user) }
+  let(:question) { create(:question, user: author) }
+
+  it 'check author of resource' do
+    expect(author.author_of?(question)).to be_truthy
+    expect(user.author_of?(question)).to be_falsey
+  end
 end
