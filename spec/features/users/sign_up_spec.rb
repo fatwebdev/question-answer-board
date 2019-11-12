fcb7bdded2ddae4e98d1558ca84353cb3bbed623
Yan Shevtsov <fatwebdev@gmail.com>
2019-11-12 11:43:35 +0300

    add spec to sign up feature spec

--- spec/features/users/sign_up_spec.rb
+++ spec/features/users/sign_up_spec.rb
@@ -5,28 +5,43 @@ feature 'User can sign up', '
   As an unauthenticated user
   I\'d like to be able to sign up
 ' do
-  background do
-    visit root_path
-    click_on 'Sign Up'
+  given!(:user) { create(:user) }
+
+  context 'as not authorized user' do
+    background do
+      visit root_path
+      click_on 'Sign Up'
+    end
+
+    scenario 'User tries to sign up' do
+      fill_in 'Email', with: 'user@test.com'
+      fill_in 'Password', with: '12345678'
+      fill_in 'Password confirmation', with: '12345678'
+      click_on 'Sign up'
+
+      expect(page).to have_content 'Welcome! You have signed up successfully.'
+    end
+
+    scenario 'User tries to sign up with registred email' do
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: '12345678'
+      fill_in 'Password confirmation', with: '12345678'
+      click_on 'Sign up'
+
+      expect(page).to have_content 'Email has already been taken'
+    end
   end
 
-  scenario 'User tries to sign in' do
-    fill_in 'Email', with: 'user@test.com'
-    fill_in 'Password', with: '12345678'
-    fill_in 'Password confirmation', with: '12345678'
-    click_on 'Sign up'
+  context 'as authorized user' do
+    background do
+      login_as(user)
+      visit root_path
+    end
 
-    expect(page).to have_content 'Welcome! You have signed up successfully.'
-  end
-
-  scenario 'User tries to sign in with registred email' do
-    user = create(:user)
-
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: '12345678'
-    fill_in 'Password confirmation', with: '12345678'
-    click_on 'Sign up'
-
-    expect(page).to have_content 'Email has already been taken'
+    scenario 'does not able to sign up' do
+      expect(page).to_not have_content 'Sign Up'
+      visit new_user_registration_path
+      expect(page).to have_content 'You are already signed in.'
+    end
   end
 end
