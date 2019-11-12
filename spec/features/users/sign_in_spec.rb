9c805966121ac718d691826a55274d4300e76ed7
Yan Shevtsov <fatwebdev@gmail.com>
2019-11-12 11:23:24 +0300

    add spec to sign in feature spec

--- spec/features/users/sign_in_spec.rb
+++ spec/features/users/sign_in_spec.rb
@@ -12,19 +12,32 @@ feature 'User can sign in', '
     click_on 'Sign In'
   end
 
-  scenario 'Registered user tries to sign in' do
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: user.password
-    click_on 'Log in'
+  context 'as registered user' do
+    background do
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: user.password
+      click_on 'Log in'
+    end
 
-    expect(page).to have_content 'Signed in successfully'
+    scenario 'tries to sign in' do
+      expect(page).to have_content 'Signed in successfully'
+    end
+
+    scenario 'does not able to sign in if already sign in' do
+      expect(page).to_not have_content 'Sign in'
+
+      visit new_user_session_path
+      expect(page).to have_content 'You are already signed in.'
+    end
   end
 
-  scenario 'Unregistered user tries to sign in' do
-    fill_in 'Email', with: 'wrong@test.com'
-    fill_in 'Password', with: '12345678'
-    click_on 'Log in'
+  context 'as not registered user' do
+    scenario 'does not able to sign in' do
+      fill_in 'Email', with: 'wrong@test.com'
+      fill_in 'Password', with: '12345678'
+      click_on 'Log in'
 
-    expect(page).to have_content 'Invalid Email or password'
+      expect(page).to have_content 'Invalid Email or password'
+    end
   end
 end
